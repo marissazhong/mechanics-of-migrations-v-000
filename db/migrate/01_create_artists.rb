88578ee7ad380be32dde7671a532c76b91a98971
Marissa Zhong <mwz2101@columbia.edu>
2018-07-09 19:18:30 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -4,4 +4,12 @@ class CreateArtists < ActiveRecord::Migrations
 
   def down
   end
+
+  def change
+    create_table :artists {|t|
+      t.string :name
+      t:string :genre
+      t.integer :age
+      t.string :hometown
+    }
 end
